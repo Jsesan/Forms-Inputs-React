65752b201a6a485dff6c9fbf99bd8fdc78a90933
Jsesan <jsesan00@gmail.com>
2021-08-12 16:22:29 -0300

    ref validation and basic validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,22 +1,35 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
-  const[enteredName, setEnteredName] = useState('');
+  const inputNameRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
 
-  const nameInputChangeHandler = event =>{
+  const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
 
-  const formSubmissionHandler = event => {
+  const formSubmissionHandler = (event) => {
     event.preventDefault();
+
+    if(enteredName.trim().length === 0){
+      return;
+    }
     console.log(enteredName);
+    setEnteredName("");
+    console.log(inputNameRef.current.value);
   };
 
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={nameInputChangeHandler} />
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={inputNameRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
